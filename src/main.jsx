7338b068ce25ddf1c33b725955a6dcd863558623
sarthakbombale <sarthakbomble250@gmail.com>
2025-11-03 23:01:20 +0530

    Added colorful bouncing loader before app renders

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
+
+// fade out preloader
+window.addEventListener('load', () => {
+  const preloader = document.getElementById('preloader')
+  if (preloader) {
+    preloader.classList.add('fade-out')
+    setTimeout(() => preloader.remove(), 600)
+  }
+})
